e8618a91a8330ad6085db388b527be7bed9eb71b
Joseph Vitko <josephvitko21@gmail.com>
2020-12-22 17:51:57 -0500

    integrated RescueTime

---
+++ RescueTime_API.py
@@ -0,0 +1,35 @@
+import datetime
+import requests
+from pprint import pprint
+
+
+class RescueTime_API:
+    def __init__(self, key):
+        self.key = key
+
+    def get_time_data(self, day=str(datetime.datetime.today())[:10]):
+        payload = {'key': self.key,
+                   'perspective': 'interval',
+                   'restrict_kind': 'productivity',
+                   'interval': 'day',
+                   'restrict_begin': day,
+                   'restrict_end': day,
+                   'format': 'json'
+                   }
+        response = requests.get('https://www.rescuetime.com/anapi/data', params=payload)
+        data = response.json().get('rows')
+        day_prods = [0, 0, 0, 0, 0]
+        for row in data:
+            level = row[3]
+            day_prods[level + 2] = row[1] / 3600.0
+        return day_prods
+
+    def get_net_productivity_today(self):
+        data = self.get_time_data()
+        semi_factor = 0.5
+        productive_time = data[4] + data[3] * semi_factor
+        unproductive_time = data[0] + data[1] * semi_factor
+        return productive_time - unproductive_time
+
+
+
---
+++ productivity_light.py
@@ -0,0 +1,22 @@
+from LIFX_API import LIFX_API
+from RescueTime_API import RescueTime_API
+import time
+import datetime
+
+
+def productivity_light(lifx_token, rt_token, log=False):
+    api = LIFX_API(token=lifx_token)
+    rt_api = RescueTime_API(rt_token)
+
+    light = api.list_lights()[0]
+    while True:
+        net_productivity = rt_api.get_net_productivity_today()  # in hours
+        scale = 10  # hours for max effect
+        strength = abs(net_productivity / scale)
+
+        if net_productivity < 0:
+            light.modify_color(hue=0, saturation=strength)
+        else:
+            light.modify_color(hue=210, saturation=strength)
+        if log: print(str(datetime.datetime.today()) + ", " + str(light.color))
+        time.sleep(300)
